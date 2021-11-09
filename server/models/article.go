4ea3c37d21946bd8664833380134916ccfcaab0b
ffichtenmueller <frank.fichtenmueller@outlook.com>
2021-11-09 21:26:58 +0400

    adds Article Model

--- server/models/article.go
+++ server/models/article.go
@@ -1,12 +1,24 @@
 package models
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type Article struct {
-	ID     uint   `json:"id" gorm:"primary_key"`
-	Title  string `json:"title"`
-	Author string `json:"author"`
+	gorm.Model
+	Title     string    `json:"title"`
+	Subtitle  string    `json:"subtitle"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Author    Author    `gorm:"embedded";embeddedPrefix:author_`
 }
 
 type CreateArticleInput struct {
-	Title  string `json:"title" binding:"required"`
-	Author string `json:"author" binding:"required"`
+	Title    string `json:"title" binding:"required"`
+	Author   Author `gorm:"embedded";embeddedPrefix:author_`
+	Subtitle string `json:"subtitle" binding:"required"`
+	Content  string `json:"content" binding:"required"`
 }
